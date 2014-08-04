44a85192101627421b60ca8c11829c604ae26209
Joe Frizzell <joe.frizzell@us.penguingroup.com>
2014-08-03 23:11:34 -0400

    json?

--- CoC/php/getuser.php
+++ CoC/php/getuser.php
@@ -6,29 +6,6 @@ if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 $sql="SELECT * FROM Users WHERE Username = '".$q."'";
-$result = mysqli_query($con,$sql);
-
-echo "<table border='1'>
-<tr>
-<th>UserName</th>
-<th>THLevel</th>
-<th>MaxBarracks</th>
-<th>MaxDarkBarracks</th>
-<th>LaboratoryLevel</th>
-<th>SpellFactoryLevel</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['UserName'] . "</td>";
-  echo "<td>" . $row['THLevel'] . "</td>";
-  echo "<td>" . $row['MaxBarracks'] . "</td>";
-  echo "<td>" . $row['MaxDarkBarracks'] . "</td>";
-  echo "<td>" . $row['LaboratoryLevel'] . "</td>";
-  echo "<td>" . $row['SpellFactoryLevel'] . "</td>";
-  echo "</tr>";
-}
-echo "</table>";
-
+echo json_encode(mysqli_query($con,$sql));
 mysqli_close($con);
 ?>
