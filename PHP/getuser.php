3c033a73f9613c725b3cb0c5f1d21e2a7c3cbc2d
Joe Frizzell <joe.frizzell@us.penguingroup.com>
2014-08-10 03:43:08 -0400

    blah

--- PHP/getuser.php
+++ PHP/getuser.php
@@ -1,27 +1,25 @@
 <?php
 $q = intval($_GET['q']);
 
-$con = mysqli_connect('10.140.143.26:3306','root','','CoC_Ultimate');
+$con = mysqli_connect('69.114.118.216:3306','CoC','Tracker4$','CoC_Ultimate');
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 
 mysqli_select_db($con,"CoC_Ultimate");
-$sql="SELECT * FROM user WHERE id = '".$q."'";
+$sql="SELECT * FROM Users WHERE UserName = '".$q."'";
 $result = mysqli_query($con,$sql);
 
-echo "<table border='1'>
-<tr>
-<th>Firstname</th>
-<th>THLevel</th>
-<th>ClanCastleLevel</th>
-</tr>";
+echo "<table border='1'><tr><th>UserName</th><th>UserLvl</th><th>GoldAmount</th><th>ElixirAmount</th><th>DarkElixirAmount</th><th>Gems</th></tr>";
 
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
-  echo "<td>" . $row['FirstName'] . "</td>";
-  echo "<td>" . $row['THLevel'] . "</td>";
-  echo "<td>" . $row['ClanCastleLevel'] . "</td>";
+  echo "<td>" . $row['UserName'] . "</td>";
+  echo "<td>" . $row['UserLvl'] . "</td>";
+  echo "<td>" . $row['GoldAmount'] . "</td>";
+  echo "<td>" . $row['ElixirAmount'] . "</td>";
+  echo "<td>" . $row['DarkElixirAmount'] . "</td>";
+  echo "<td>" . $row['Gems'] . "</td>";
   echo "</tr>";
 }
 echo "</table>";
