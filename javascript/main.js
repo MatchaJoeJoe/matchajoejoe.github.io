3570b2fe94455bac66d73b6dbba229879f945646
Joe Frizzell <joe.frizzell@us.penguingroup.com>
2014-11-18 16:04:52 -0500

    updating and updating

--- javascript/main.js
+++ javascript/main.js
@@ -1,64 +1,80 @@
 window.addEventListener('load', eventWindowLoaded, false);	
 function eventWindowLoaded() {
 	var LoadingBackground = document.getElementById('LoadingBackground');
-	var LoadingContainer = document.getElementById('LoadingContainer');
 	var JoeContainer = document.getElementById('JoeContainer');
     var user=getCookie("username");
-	LoadingContainer.innerHTML='';
-    if (user != "") {
+	LoadingBackground.parentNode.removeChild(LoadingBackground);
+	var containerDiv = document.getElementById('container');
+	var blockerDiv = document.createElement("div");
+	blockerDiv.id = 'blocker';
+	containerDiv.appendChild(blockerDiv);
+	var twitterDiv = document.createElement("div");
+	twitterDiv.id = 'twitterwrapper';
+	twitterDiv.className = 'hidden';
+	twitterDiv.innerHTML = '<a class="twitter-timeline" href="https://twitter.com/JoeFrizzell" data-widget-id="312040975352602624">Tweets by @JoeFrizzell</a>';
+	containerDiv.appendChild(twitterDiv);
+	setTimeout(function() {
+		loadTwitter();
+	}, 1000);
+	if (user != "") {
 		setTimeout(function() {
-			JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble fadein">Hey '+user+'.<br/>&#160;</span></div>';
+			theText = 'Hey '+user+'.';
+			startTalking(theText);
+			setTimeout(function() {
+				stopTalking(theText);
+			}, 1500);
 		}, 2000);
 		setTimeout(function() {
-			JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble"><span class="fadeout" >Hey '+user+'.</span><br/><span class="fadein" >Welcome back.</span></span></div>';
+			JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeMouth" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble"><span class="fadeout" >Hey '+user+'.</span><br/><span class="fadein" >Welcome back.</span></span></div>';
 		}, 4000);
 		setTimeout(function() {
-			JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble fadeout">&#160;<br/><span class="fadeout" >Welcome back.</span></span></div>';
-			LoadingBackground.className = 'fadeout';
+			JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble fadeout">&#160;<br/><span class="fadeout" >Welcome back.</span></span></div>';
+			blockerDiv.parentNode.removeChild(blockerDiv);
 		}, 6000);
+    } 
+    else {
 		setTimeout(function() {
-			LoadingBackground.parentNode.removeChild(LoadingBackground);
-			JoeContainer.removeChild(document.getElementById('bubbleBox'));
-		}, 8000);
-    } else {
-		setTimeout(function() {
-			JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble fadein">Hi, I’m Joe.<br/>&#160;<br/>&#160;<br/>&#160;</span></div>';
+			theText = 'Hi, I’m Joe.';
+			startTalking(theText);
+			setTimeout(function() {
+				stopTalking(theText);
+			}, 1400);
 		}, 2000);
 		setTimeout(function() {
-			JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble"><span class="fadeout">Hi, I’m Joe.</span><br/><span class="fadein" >Welcome to my site.</span><br/>&#160;<br/>&#160;</span></div>'
-		}, 4000);
+			theText = 'Welcome to my site.';
+			startTalking(theText);
+			setTimeout(function() {
+				stopTalking(theText);
+			}, 2000);
+		}, 3900);
 		setTimeout(function() {
-			JoeContainer.innerHTML='<span class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble">&#160;<br/><span class="fadeout" >Welcome to my site.</span><br/><span class="fadein">What’s your name?<br/>&#160;</span></span></span>';
-		}, 6000);
-		setTimeout(function() {
-			JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble">&#160;<br/><br/>What’s your name?<br/><span class="fadein"><input autofocus="autofocus" type="text" id="NewName" onkeydown="if (event.keyCode == 13) {setUsername(\'username\', this, 30);};"/></span></span></div>';
-		}, 8000);
+			theText = 'What’s your name?<br/><input autofocus="autofocus" type="text" id="NewName" onkeydown="if (event.keyCode == 13) {setUsername(\'username\', this, 30);};"/>';
+			startTalking(theText);
+			setTimeout(function() {
+				document.getElementById('JoeMouth').className = 'hidden';
+			}, 1400);
+			blockerDiv.parentNode.removeChild(blockerDiv);
+		}, 6400);
     }	
-	loadContent();
 }
 function setUsername(cname, theInput,exdays) {
 	var LoadingBackground = document.getElementById('LoadingBackground');
 	var LoadingContainer = document.getElementById('LoadingContainer');
-	var cvalue = theInput.value
+	var cvalue = theInput.value;
     if (cvalue != "") {
-    	cvalue = ' ' +cvalue
+    	cvalue = ' ' +cvalue;
     }
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname+"="+cvalue+"; "+expires;
-	JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble">&#160;<br/><br/><span class="fadeout">What’s your name?<br/>'+cvalue+'&#160;</span></span></div>';
+	JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble">&#160;<br/><br/><span class="fadeout">What’s your name?<br/>'+cvalue+'&#160;</span></span></div>';
 	setTimeout(function() {
-		JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble">&#160;<br/><span class="fadein" >Nice to meet you'+cvalue+'.</span><br/><br/>&#160;</span></div>';
+		JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeMouth" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble">&#160;<br/><span class="fadein" >Nice to meet you'+cvalue+'.</span><br/><br/>&#160;</span></div>';
 	}, 1000);
 	setTimeout(function() {
-		JoeContainer.innerHTML='<div class="frontAndCenter" ><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble fadeout">&#160;<br/>Nice to meet you'+cvalue+'.<br/><br/>&#160;</span></div>';
-		LoadingBackground.className = 'fadeout';
+		JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble fadeout">&#160;<br/>Nice to meet you'+cvalue+'.<br/><br/>&#160;</span></div>';
 	}, 4000);
-	setTimeout(function() {
-		LoadingBackground.parentNode.removeChild(LoadingBackground);
-		JoeContainer.removeChild(document.getElementById('bubbleBox'));
-	}, 6000);
 }
 
 function getCookie(cname) {
@@ -74,103 +90,126 @@ function getCookie(cname) {
     return "";
 }
 
-function makeTabActive(currentTab){
-	var activeTabs = document.getElementsByClassName('activetab'), i = activeTabs.length;
-    while(i--) {
-		activeTabs[i].className = 'tab';
+function loadContent(theCaller){
+	var theText = "...";
+	var JoeContainer = document.getElementById('JoeContainer');
+	var callerID = theCaller.id;
+	var containerDiv = document.getElementById('container');
+	var tabTest = document.getElementById('content');
+	if(tabTest == null){
+		var contentDiv = document.createElement("div");
+		contentDiv.id = 'content';
+		containerDiv.appendChild(contentDiv);
+		contentDiv.className = 'hidden'; 
+		contentDiv.onclick = function () {
+			hideContent();
+		}
 	}
-	currentTab.className='activetab glow';
-}
-
-function loadContent(){
-	var activeTabs = document.getElementsByClassName('activetab'), i = activeTabs.length;
-	var tabContentDiv = document.getElementById('tabContent');
-	var twitterFeed = document.getElementById('twitterwrapper');
+	else {
+		var contentDiv = document.getElementById('content');
+	}
+	var tabClass = contentDiv.className;
+	if (tabClass !='hidden'){
+	contentDiv.className = "quickfadeout";
+		setTimeout(function() {
+			contentDiv.className = "hidden";
+		}, 500);
+	}
+	if (callerID.lastIndexOf("chair")===0){
+		theText = 'There is nothing better than curling up in a comfy chair and reading... or watching YouTube videos.';
+		startTalking(theText);
+		setTimeout(function() {
+			stopTalking(theText);
+			contentDiv.className = "quickfadein";
+			contentDiv.innerHTML = '<div class="videoWrapper"><iframe width="100%" height="100%" src="http://www.youtube.com/embed/H-0GKrFG8kw?list=UUTDKXx4nSKZ9CGyeHyb9m4w" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>';
+		}, 4000);
+	}
+	if (callerID.lastIndexOf("laptop")===0){
+		theText = 'I spend ridiculous amounts of time working on my laptop... Working, yeah, that’s what I’m doing.';
+		startTalking(theText);
+		setTimeout(function() {
+			stopTalking(theText);
+			contentDiv.className = "quickfadein";
+			contentDiv.innerHTML = '<img id="resume" onclick="OpenInNewTab(\'http://joefrizzell.com/Frizzell-Joe_resume.pdf\')" src="images/Frizzell-Joe_resume.jpg" alt="Frizzell-Joe_resume" />';
+		}, 4000);
+	}
+	if (callerID.lastIndexOf("phone")===0){
+		theText = 'Isn’t it great that we are always connected with anyone we’ve ever met? Isn’t it??';
+		startTalking(theText);
+		setTimeout(function() {
+			stopTalking(theText);
+			contentDiv.className = "quickfadein";
+			var twitterwrapper = document.getElementById('twitterwrapper');
+			twitterwrapper.className = "quickfadein";
+		}, 3000);
+	}
+	if (callerID.lastIndexOf("art")===0){
+		theText = 'Do you like art? I like t-shirts.';
+		startTalking(theText);
+		setTimeout(function() {
+			stopTalking(theText);
+			contentDiv.className = "quickfadein";
+			contentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="portfolio.html"/>';
+		}, 3000);
+	}
+	if (callerID.lastIndexOf("book")===0){
+		setTimeout(function() {
+			contentDiv.className = "quickfadein";
+			contentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="coloringBook/index.xhtml"/>';
+		}, 500);
+	}
+	if (callerID.lastIndexOf("raven")===0){
+		setTimeout(function() {
+			contentDiv.className = "quickfadein";
+			contentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="raven-colors/index.html"/>';
+		}, 500);
+	}
 	
-    while(i--) {
-		var TabContent = activeTabs[i].textContent;
-	}
-	if (TabContent.lastIndexOf("Feed")===0){
-		tabContentDiv.className = "quickfadeout";
+	/*Just for fun*/
+	if (callerID.lastIndexOf("table")===0){
+		theText = 'That is just a coffee table.';
+		startTalking(theText);
 		setTimeout(function() {
-			twitterFeed.className = "quickfadein";
-			twitterFeed.style.zIndex = '98';
-			tabContentDiv.className = "quickfadein";
-			tabContentDiv.innerHTML = '<div class="w50r"><div class="videowrapper"><iframe width="100%" height="100%" src="http://www.youtube.com/embed/H-0GKrFG8kw?list=UUTDKXx4nSKZ9CGyeHyb9m4w" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div></div>';
-		}, 500);
+			stopTalking(theText);
+		}, 1500);
 	}
-	if (TabContent.lastIndexOf("About")===0){
-		tabContentDiv.className = "quickfadeout";
-		twitterFeed.className = "quickfadeout";
+	if (callerID.lastIndexOf("floor")===0){
+		theText = 'Do you like my floors? They’re nice, right?';
+		startTalking(theText);
 		setTimeout(function() {
-			tabContentDiv.className = "quickfadein";
-			twitterFeed.style.zIndex = '-1';
-			tabContentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="about.html"/>';
-		}, 500);
+			stopTalking(theText);
+		}, 1501);
 	}
-	if (TabContent.lastIndexOf("Resume")===0){
-		tabContentDiv.className = "quickfadeout";
-		twitterFeed.className = "quickfadeout";
-		setTimeout(function() {
-			tabContentDiv.className = "quickfadein";
-			twitterFeed.style.zIndex = '-1';
-			tabContentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="Frizzell-Joe_resume.pdf"/>';
-		}, 500);
+}
+function hideContent(){
+	var twitterwrapper = document.getElementById('twitterwrapper');
+	var twitterClass = twitterwrapper.className;
+	if (twitterClass !='hidden'){
+		twitterwrapper.className = 'quickfadeout';
 	}
-	if (TabContent.lastIndexOf("Portfolio")===0){
-		tabContentDiv.className = "quickfadeout";
-		twitterFeed.className = "quickfadeout";
-		setTimeout(function() {
-			tabContentDiv.className = "quickfadein";
-			twitterFeed.style.zIndex = '-1';
-			tabContentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="portfolio.html"/>';
-		}, 500);
-	}
-	if (TabContent.lastIndexOf("Coloring Book")===0){
-		tabContentDiv.className = "quickfadeout";
-		twitterFeed.className = "quickfadeout";
-		setTimeout(function() {
-			tabContentDiv.className = "quickfadein";
-			twitterFeed.style.zIndex = '-1';
-			tabContentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="coloringBook/index.xhtml"/>';
-		}, 500);
-	}
-	if (TabContent.lastIndexOf("Raven’s Colors")===0){
-		tabContentDiv.className = "quickfadeout";
-		twitterFeed.className = "quickfadeout";
-		setTimeout(function() {
-			tabContentDiv.className = "quickfadein";
-			twitterFeed.style.zIndex = '-1';
-			tabContentDiv.innerHTML = '<iframe class="w90" frameborder="0" src="raven-colors/index.html"/>';
-		}, 500);
+	var contentDiv = document.getElementById('content');
+	var tabClass = contentDiv.className;
+	if (tabClass !='hidden'){
+		contentDiv.className = 'quickfadeout';
 	}
 	setTimeout(function() {
-		loadTwitter();
-	}, 10);
-
-}
-
-function loadSubContent(currentButton){
-	var theSubContent = currentButton.innerHTML;
-	var theContainer = document.getElementById('tabContent');
-	var greydiv = document.createElement('div');
-	greydiv.id= 'greyedout';
-	theContainer.appendChild(greydiv);
-	var NewDiv = document.createElement('div');
-	NewDiv.id = 'PopUpWindow';
-	NewDiv.innerHTML = '<span id="backButton" onclick="closePopUpWindow();">Back</span><span class="button" >'+theSubContent+'</span>';
-	theContainer.appendChild(NewDiv);
-}
-function closePopUpWindow(){
-	var greydiv = document.getElementById('greyedout');
-	greydiv.parentNode.removeChild(greydiv);
-	var NewAccountPage = document.getElementById('PopUpWindow');
-	NewAccountPage.parentNode.removeChild(NewAccountPage);
+		twitterwrapper.className = 'hidden';
+		contentDiv.innerHTML = '';
+		contentDiv.parentNode.removeChild(contentDiv);
+	}, 500);
 }
 function OpenInNewTab(url) {
   var win = window.open(url, '_blank');
   win.focus();
 }
 function loadTwitter(){
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");	var tabContentDiv = document.getElementById('tabContent');
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+}
+function startTalking(theText){
+	var JoeContainer = document.getElementById('JoeContainer');
+	JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeMouth" src="images/Joe8Bit_talking.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble quickfadein">'+theText+'</span></div>';
+}
+function stopTalking(theText){
+	var JoeContainer = document.getElementById('JoeContainer');
+	JoeContainer.innerHTML='<div><img id="JoeBody" src="images/Joe8Bit.gif" alt=""/><img id="JoeEyes" src="images/Joe8Bit_eyes_forward.gif" alt=""/><span id="bubbleBox" class="speechBubble quickfadeout">'+theText+'</span></div>';
 }
