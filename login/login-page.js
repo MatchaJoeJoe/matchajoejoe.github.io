50b20e5c123dc276316251dcf03f21f23d4cd325
Joe <joefrizzell@gmail.com>
2020-11-20 20:17:35 -0500

    oops

--- login/login-page.js
+++ login/login-page.js
@@ -4,7 +4,7 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  document.getElementsByClassName('className')[0].style.display = "block";
+  document.getElementsByClassName('signout')[0].style.display = "block";
 }
 
 function signOut() {
